7efc45e6def0711cef183d43ff92a0e64f2ca245
Florencia Guerrero <70726723+fr21g@users.noreply.github.com>
2022-04-11 19:04:20 -0300

    Add files via upload

---
+++ ejercicio10.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=
+    , initial-scale=1.0">
+     <!--    <link rel="stylesheet" href="bootstrap-5.1.-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="bootstrap-5.1.-dist/js/bootstrap.bundle.min.js"> -->
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../js/bootstrap.bundle.min.js">
+    <title>Ejercicio 10</title>
+</head>
+<body>
+<div class="container">
+<div class="row" >
+<div class="col" style="background-color:greenyellow">Header</div>
+</div>    
+<div class="row">
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 d-none d-lg-block" ></div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 " style="background-color:hotpink">1</div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6" style="background-color:hotpink">2</div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 d-none d-lg-block" ></div>
+    
+</div>
+<div class="row">
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6" style="background-color:hotpink">3</div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 d-none d-lg-block"></div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6 d-none d-lg-block"></div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6" style="background-color:hotpink">4</div>
+</div>
+<div class="row">
+    <div class="col d-none d-md-block d-lg-none" style="background-color:orange">Tablet</div>
+</div>
+<div class="row">
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6" style="background-color:#00FFFF">a</div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6" style="background-color:#00FFFF">b</div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6" style="background-color:#00FFFF">c</div>
+    <div class="col-xl-3 col-lg-3 col-md-3 col-sm-6" style="background-color:#00FFFF">d</div>
+</div>
+
+<div class="row ">
+    <div class="col d-none d-lg-block   " style="background-color:orange">Desktop</div>
+</div>
+<div class="row ">
+    <div class="col d-none d-sm-block d-md-none" style="background-color:orange">Mobile</div>
+</div>
+
+<div class="row">
+    <div class="col d-none d-lg-block"></div>
+    <div class="col d-sm-none d-md-block" style="background-color:grey">:(</div>
+    <div class="col" style="background-color:grey">:D</div>
+    <div class="col d-none d-lg-block"></div>
+</div>
+
+<div class="row " >
+    <div class="col d-none d-sm-block" style="background-color:greenyellow">Footer</div>
+</div>
+
+
+
+</div>
+
+</body>
+</html>
